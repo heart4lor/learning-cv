db2ac2a540afd519d4cba7a2fe96e936fedfe39d
syf <syf-98@hotmail.com>
2018-05-21 15:37:15 +0800

    sync

--- blur.py
+++ blur.py
@@ -2,10 +2,10 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('in/cvlogo.png')
+img = cv2.imread('in/cvlogo_noisy.png')
 # img[:][:][0], img[:][:][2] = img[:][:][2], img[:][:][0]
 
-blur = cv2.medianBlur(img, 5)
+blur = cv2.bilateralFilter(img, 9, 75, 75)
 
 plt.subplot(121), plt.imshow(img), plt.title('Original')
 plt.xticks([]), plt.yticks([])
